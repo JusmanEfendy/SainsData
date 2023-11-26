211a83cdce1011608e484feb715db1f54304c5e6
JusmanEfendy <njhussrhiee0206@gmail.com>
2023-11-26 20:48:45 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminLoginSeeder::class,
             DusunSeeder::class,
+            PosyanduSeeder::class,
         ]);
     }
 }
