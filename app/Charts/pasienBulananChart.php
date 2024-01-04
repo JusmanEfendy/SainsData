454ffe7c50bb991485572e78873a60026f3406d4
Jussy <njhussrhiee0206@gmail.com>
2024-01-04 15:31:03 +0700

    Update Dummy LineChart

--- app/Charts/pasienBulananChart.php
+++ app/Charts/pasienBulananChart.php
@@ -27,11 +27,24 @@ class pasienBulananChart
         $data[] = $datas->count();
         $months[] = $datas->created_at->format('M');
         }
+        // return $this->chart->lineChart()
+        //         ->addData('data', $data)
+        //         ->setXaxis($months)
+        //         ->setTitle('Pasien Bulanan')
+        //         ->setFontColor('#FFF')
+        //         ->setLabels($months);
+
         return $this->chart->lineChart()
-                ->addData('data', $data)
-                ->setXaxis($months)
-                ->setTitle('Pasien Bulanan')
-                ->setFontColor('#FFF')
-                ->setLabels($months);
+    ->setTitle('Sales during 2021.')
+    ->setSubtitle('Physical sales vs Digital sales.')
+    ->setFontColor('#FFF')
+    ->addData('Published posts', [4, 9, 5, 2, 1, 8, 3, 9, 6, 5, 7, 3])
+    ->addData('Unpublished posts', [7, 2, 7, 2, 5, 4, 8, 3, 9, 1, 6, 9])
+    ->addData('Unpublished posts', [7, 2, 7, 2, 5, 4, 8, 3, 9, 1, 6, 9])
+    ->addData('Unpublished posts', [7, 2, 7, 2, 5, 4, 8, 3, 9, 1, 6, 9])
+    ->addData('Unpublished posts', [7, 2, 7, 2, 5, 4, 8, 3, 9, 1, 6, 9])
+    ->addData('Unpublished posts', [7, 2, 7, 2, 5, 4, 8, 3, 9, 1, 6, 9])
+    ->setColors(['#ffc63b', '#ff6384'])
+    ->setXAxis(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agustus', 'September', 'Oktober', 'November', 'Desember']);
     }
 }
